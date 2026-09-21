abd9dd9a967caba7b71391ed3005cd92a49a9456
GraysonSeven <93363745+GraysonSeven@users.noreply.github.com>
2026-09-21 11:35:14 +0800

    build: mirror Clientbound 1.1 app version

--- clientbound-src/lib/app_constants.dart
+++ clientbound-src/lib/app_constants.dart
@@ -1,4 +1,4 @@
-const clientboundVersion = '1.0.0+10';
+const clientboundVersion = '1.1.0+11';
 const clientboundWebUrl = 'https://graysonseven.github.io/clientbound/';
 const clientboundReleaseUrl =
     'https://github.com/GraysonSeven/graysonseven.github.io/releases';
